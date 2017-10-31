392edcd3f733ed094aa25bf6f0f96cd5417b451a
msmenegol <marcelosmenegol@gmail.com>
2017-10-31 17:13:17 -0200

    Change EOF to /n

--- ESP.py
+++ ESP.py
@@ -1,14 +1,21 @@
 import serial
 
 class ESP:
-    def __init__(self, port, baud_rate=115200, timeout=0.1):
+    def __init__(self, port, name, baud_rate=115200, timeout=0.1):
         self._port = port
+        self._name = name
+        self._ID = ''
         self.interface = serial.Serial(port, baud_rate, timeout = timeout)
 
         self.send('ID_REQ')
-        self._ID = ''
         while self._ID == '':
             self._ID = self.read()
+        self.send(name)
+        ok = ''
+        while ok != 'NAME_OK':
+            print(ok)
+            ok = self.read()
+
 
     def _encode(self, msg):
         return msg + '\n'
@@ -17,7 +24,7 @@ class ESP:
         self.interface.write(self._encode(msg))
 
     def read(self):
-        return self.interface.readline()
+        return self.interface.readline()[:-1]
 
     def getID(self):
         return self._ID
